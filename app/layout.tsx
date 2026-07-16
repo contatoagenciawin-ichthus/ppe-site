d54928b435a34e59899fe6ec7f88b3ee2a8e7773
Ichthus Mkt - Agência Win <contatoagenciawin@gmail.com>
2026-07-16 10:41:48 -0300

    Integrate Facebook Pixel tracking script
    Added Facebook Pixel tracking script for analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next"
+import Script from "next/script"
 import { Geist_Mono, Poppins, Sora, Urbanist } from "next/font/google"
 import "./globals.css"
 
@@ -39,7 +40,33 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body
         className={`${sora.variable} ${poppins.variable} ${urbanist.variable} ${geistMono.variable} antialiased`}
-      >
+      <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2536620450142314');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        
+        
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2536620450142314&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
