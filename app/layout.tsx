4edaca6a2d8f6c03791e8a213b0415ec988d9c46
Ichthus Mkt - Agência Win <contatoagenciawin@gmail.com>
2026-08-25 11:20:12 -0300

    Skip Clarity inside Meta in-app browsers

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,14 @@ export default function RootLayout({
         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
             (function(c,l,a,r,i,t,y){
+              var isMetaInAppBrowser = /FBAN|FBAV|FB_IAB|Instagram|Messenger/i.test(
+                navigator.userAgent || ""
+              );
+
+              if (isMetaInAppBrowser) {
+                return;
+              }
+
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
